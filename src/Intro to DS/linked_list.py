e1c32acff6d483e189f739c6d4ed2e9cdb061b94
singfungcheung <singfung.cheung@gmail.com>
2024-03-26 21:57:13 -0400

    implemented remove

--- src/Intro to DS/linked_list.py
+++ src/Intro to DS/linked_list.py
@@ -89,6 +89,30 @@ class LinkedList:
 
             prev_node.next_node = new
             new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key
+        Return the node or None if key doesn't exist
+        Takes O(n) time.
+        """
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
+
     def __repr__(self):
         """
         Return a string representation of the list
