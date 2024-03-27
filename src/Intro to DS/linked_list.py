af25a02d25590fce3558fab9bf433ce4e35e6ba8
singfungcheung <singfung.cheung@gmail.com>
2024-03-26 21:31:54 -0400

    Implemented insert

--- src/Intro to DS/linked_list.py
+++ src/Intro to DS/linked_list.py
@@ -47,6 +47,48 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key.
+        Return the node or 'None' if not found
+
+        Takes O(n) time
+        """
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            current = current.next_node
+
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position.
+        Insertion takes O(1) time but finding the node at the
+        insertion point takes O(n).
+
+        Takes overall O(n).
+        """
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
     def __repr__(self):
         """
         Return a string representation of the list
