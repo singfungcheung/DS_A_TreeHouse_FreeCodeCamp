d0adb85ad9f9df1aad61b5ebaf9e0180e2bc7a19
singfungcheung <singfung.cheung@gmail.com>
2024-03-27 23:40:00 -0400

    Started a merge sort for linked list

---
+++ src/Intro to DS/linked_list_merge_sort.py
@@ -0,0 +1,14 @@
+from linked_list import LinkedList
+
+def merge_sort(linked_list):
+    """
+    Sorts a linked list in ascending order
+    - Recursively divide the linked list into sublists containing a single node
+    - Repeatedly merge the sublists to produce sorted sublists until one remains
+
+    Returns a sorted linked list
+    """
+
+    if linked_list.size() == 1 or linked_list is None:
+        return linked_list
+    
