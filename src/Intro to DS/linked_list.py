2d0df3eb9ad7e9fb3c94aef2a99dcc94e82d5306
singfungcheung <singfung.cheung@gmail.com>
2024-03-28 23:53:16 -0400

    Implementing linked list merge and made some modifications to linked list

--- src/Intro to DS/linked_list.py
+++ src/Intro to DS/linked_list.py
@@ -134,6 +134,17 @@ class LinkedList:
 
         return current_node
 
+    def node_at_index(self, index):
+        if index == 0:
+            return self.head
+        else:
+            current = self.head
+            position = 0
+
+            while position < index:
+                current = current.next_node
+                position += 1
+            return current
 
     def __repr__(self):
         """
--- src/Intro to DS/linked_list_merge_sort.py
+++ src/Intro to DS/linked_list_merge_sort.py
@@ -11,4 +11,25 @@ def merge_sort(linked_list):
 
     if linked_list.size() == 1 or linked_list is None:
         return linked_list
-    
+
+    left_half, right_half = split(linked_list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+def split(linked_list):
+    """
+    divide the unsorted linked=list at midpoint into sublists.
+    """
+
+    if linked_list == None or linked_list.head == None:
+        left_half = linked_list
+        right_half = None
+
+        return left_half, right_half
+    else:
+        size = linked_list.size()
+        mid = size // 2
+
+        mid_node = linked_list.node_at_index(mid - 1)
